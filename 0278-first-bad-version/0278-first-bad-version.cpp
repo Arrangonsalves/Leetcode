d1765f87cf1872a353b40cfa6c3cac0d23ca9cbc
Arrangonsalves <63124107+Arrangonsalves@users.noreply.github.com>
2022-10-31 03:09:47 +0530

    Time: 5 ms (16.15%), Space: 5.9 MB (67.69%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -4,13 +4,15 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        for(int i=n;i>=1;i--)
+        int l=1,r=n;
+        while(l<=r)
         {
-            if(!isBadVersion(i)){
-                return i+1;
-            }
-                
+            int mid=l+(r-l)/2;
+            if(isBadVersion(mid)==true)
+                r=mid-1;
+            else
+                l=mid+1;
         }
-        return 1;
+        return l;
     }
 };
