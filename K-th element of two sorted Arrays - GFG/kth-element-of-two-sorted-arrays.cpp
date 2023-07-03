6af72a79be712830bea057e2591389e1414e724d
Arrangonsalves <63124107+Arrangonsalves@users.noreply.github.com>
2023-07-03 18:27:40 +0530

    Added solution - LeetHub

--- K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.cpp
+++ K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.cpp
@@ -8,9 +8,34 @@ class Solution{
     public:
     int kthElement(int arr1[], int arr2[], int n, int m, int k)
     {
+        //TC:log(min(n,m))
+        if(n>m)
+            kthElement(arr2,arr1,m,n,k);
+        int low=max(0,k-m),high=min(k,n);
+        
+        while(low<=high)
+        {
+            int cut1=(low+high)>>1;
+            int cut2=k-cut1;
+            
+            int l1=(cut1==0 ?INT_MIN:arr1[cut1-1]);
+            int l2=(cut2==0 ?INT_MIN:arr2[cut2-1]);
+            
+            int r1=(cut1==n ?INT_MAX:arr1[cut1]);
+            int r2=(cut2==m ?INT_MAX:arr2[cut2]);
+            
+            if((l1<=r2)&&(l2<=r1))
+                return max(l1,l2);
+            else if(l1>r2)
+                high=cut1-1;
+            else
+                low=cut1+1;
+        }
+        return 1;
+        
         
         //TC:O(M+N)
-        int i=0,j=0,k1=k;
+        /*int i=0,j=0,k1=k;
         vector<int> v;
         while(i<n && j<m)
         {
@@ -32,7 +57,7 @@ class Solution{
             if(k==0) break;
         }
        
-        return v[k1-1];
+        return v[k1-1];*/
     }
 };
 
